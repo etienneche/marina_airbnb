2c2cadde18208cecb443c1d33a2b418e9d429ee2
etienne cherri <etienne@cherri.fr>
2019-11-21 16:52:20 +0000

    connect upload photo to navbar and profile"

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2019_11_21_151531) do
 
-ActiveRecord::Schema.define(version: 2019_11_21_114105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_11_21_114105) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
