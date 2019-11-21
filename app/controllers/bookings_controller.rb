d690ef6667b8c3247333f8c529137f55799bca54
Martin Ditzel <ditzel.martin@gmail.com>
2019-11-21 13:28:55 +0000

    added booking controller, styling of show page

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,51 @@
+class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :destroy, :update]
+  before_action :set_spot, only: [:create]
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.spot = @spot
+    @booking.save
+  end
+
+  def edit
+  end
+
+  def update
+    if @booking.update(booking_params)
+      redirect_to booking_path(@booking)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_spot
+    @spot = Spot.find(params[:spot_id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_105803) do
+ActiveRecord::Schema.define(version: 2019_11_21_110509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_105803) do
     t.string "marina_name"
     t.float "latitude"
     t.float "longitude"
+    t.string "city"
     t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
